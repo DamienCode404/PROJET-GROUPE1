ccfccda5acac368b2e2b480bdefbec693571d0a6
AlexandreVlt <alexandre.valentin42@gmail.com>
2025-05-14 16:25:30 +0200

    Persistance du panier après déconnexion avec Local Storage

--- RefugeFront/src/app/panier.service.ts
+++ RefugeFront/src/app/panier.service.ts
@@ -9,15 +9,26 @@ export class PanierService {
 
   private _articles: Produit[] = [];
 
-  constructor(private produitService: ProduitService) { }
+  constructor(private produitService: ProduitService) { 
+    this.chargerPanier(); // On charge le panier au démarrage du service
+  }
 
   get articles(): Produit[] {
     return this._articles;
   }
 
+  chargerPanier() {
+    const panier = localStorage.getItem('panier');
+    if (panier) {
+      this._articles = JSON.parse(panier);
+    } else {
+      this._articles = [];
+    }
+  }
+
   ajouter(produit: Produit) {
     this._articles.push(produit);
-
+    localStorage.setItem('panier', JSON.stringify(this._articles)); // On le met à jour dans le LocalStorage
     this.produitService.decrementerStock(produit.id).subscribe({
       next: () => {
         console.log(`Stock du produit ${produit.libelle} diminué de 1`);
@@ -32,6 +43,7 @@ export class PanierService {
     const index = this._articles.findIndex(p => p.id === id);
     if (index !== -1) {
       this._articles.splice(index, 1);
+      localStorage.setItem('panier', JSON.stringify(this._articles)); // On le met à jour dans le LocalStorage
       this.produitService.restaurerStock(id).subscribe({
         next: () => {
           console.log(`Stock du produit ${id} restauré`);
@@ -59,6 +71,7 @@ export class PanierService {
     });
   
    this._articles = []; 
+   localStorage.removeItem('panier');
     console.log("Panier vidé !");
   }
 
